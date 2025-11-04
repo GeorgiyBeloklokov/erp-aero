29314bf17466fc46de65475f8b09f5cfa4ec833a
Georgiy Beloklokov <georgiy.reus@gmail.com>
2025-11-04 20:40:57 +0300

    fix: Repair ESLint v9 configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,54 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import prettier from 'eslint-plugin-prettier';
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 
 export default [
   {
+    // Global ignores
     ignores: ['dist/**', 'eslint.config.js', 'jest.config.js', '**/express.d.ts'],
+  },
+  js.configs.recommended, // Basic JS recommended rules
+  {
+    files: ['src/**/*.ts'],
     languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      parser: tseslint.parser,
+      parser: tsParser,
       parserOptions: {
         project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      ...ts.configs.recommended.rules, // TypeScript recommended rules
+      // Custom TypeScript rules
+      '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_|', caughtErrors: 'none' }],
+    },
+  },
+  {
+    files: ['src/**/*.test.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
       },
     },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettierConfig,
+  prettierConfig, // Disables ESLint rules that conflict with Prettier
   {
+    files: ['src/**/*.ts'],
     plugins: {
-      prettier: prettier,
+      prettier: prettierPlugin,
     },
     rules: {
       'prettier/prettier': 'error',
-      '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_', caughtErrors: 'none' }],
     },
   },
 ];
